4c4998491774660e42738d7b9ff6e931387b35c8
Michał <kwestorowski.m@gmail.com>
2017-10-16 22:08:58 +0200

    Initial commit

---
+++ dropdown.js
@@ -0,0 +1,7 @@
+
+
+function dropThatBeat() {
+const dropDown = document.getElementById('drop-down-button')
+const droppedMenu = document.getElementById('dropped-menu')
+    dropDown.addEventListener('click', )
+}
