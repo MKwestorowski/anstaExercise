bf248de5ace248adde1b3c77c7c13d002f0656fc
Michał <kwestorowski.m@gmail.com>
2017-10-16 22:21:47 +0200

    Basic struturce of js for button

--- dropdown.js
+++ dropdown.js
@@ -1,7 +1,20 @@
 
 
-function dropThatBeat() {
-const dropDown = document.getElementById('drop-down-button')
-const droppedMenu = document.getElementById('dropped-menu')
-    dropDown.addEventListener('click', )
+
+
+function dropDownButton () {
+    document.getElementById("myDropdown").classList.toggle("show");
 }
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        const dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            const openDropdown = dropdowns[i];
+            openDropdown.classList.contains('show') ?
+                openDropdown.classList.remove('show') : null;
+            }
+        }
+    }
